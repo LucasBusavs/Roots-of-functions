832c0b3f3d7d5bcaadf84affcff756e1a19e0261
Lucas Bueno <turmadaciencia1@gmail.com>
2022-09-11 16:36:11 -0300

    Menu is ready

--- Roots.c
+++ Roots.c
@@ -4,27 +4,60 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+char function[100];
 
 int main()
 {
     int choose;
-    char function[100];
-    int i;
+    do{
+        printf("\n\n\t\t ********************************************************************");
+        printf("\n\t\t                      WELCOME TO THE ROOTS DISCOVERER");
+        printf("\n\t\t ********************************************************************");
+        printf("\n\n\t\t 1 - Bisection Method\n");
+		printf("\n\n\t\t 2 - False Position Method\n");
+		printf("\n\n\t\t 3 - Newton Method\n");
+		printf("\n\n\t\t 4 - Secant Method\n");
+        printf("\n\n\t\t 5 - Exit\n");
+		printf("\n\n\t\t Choose an option: ");
+        scanf("%d", &choose);
+        while(choose < 1 || choose > 5){
+			printf("\n\n\t\t Choose a right option: ");
+			scanf("%d", &choose);
+		}
 
-    printf("\n\n\t\t ********************************************************************");
-    printf("\n\t\t                      WELCOME TO THE ROOTS DISCOVERER");
-    printf("\n\t\t ********************************************************************");
+
+
+    }while (choose != 5);
+  
+    
+    return 0;
+}
+
+void enterFunction(){
+
+    int i;
+    char correct;
 
     printf("\n\n\t\t Insert your function of x: ");
     scanf("%s", &function);
-
-    printf("\n\n\t\t %s", &function);
+    getchar();
 
     for ( i = 0; i < strlen(function); i++){
         function[i] = tolower(function[i]);
     }
 
-    printf("\n\n\t\t %s", &function);
-
-    return 0;
+    do{
+        printf("\n\n\t\t This is your f(x) = %s, is it correct? y/n ", &function);
+        scanf("%c", &correct);
+        correct = tolower(correct);
+        getchar();
+        if(correct != 'y'){
+            printf("\n\n\t\t Insert your function of x again: ");
+            scanf("%s", &function);
+            getchar();
+            for ( i = 0; i < strlen(function); i++){
+                function[i] = tolower(function[i]);
+             }
+        }
+    }while (correct != 'y');
 }
