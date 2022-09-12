67c5ff10fdc087c5991079d6a23e3d8d80cbe485
Lucas Bueno <turmadaciencia1@gmail.com>
2022-09-12 19:48:41 -0300

    False Position Method complete

--- Roots.c
+++ Roots.c
@@ -146,6 +146,67 @@ double xkFalsePosition(double a, double b){
     return xk;
 }
 
+void falsePosition(){
+    double a, b;
+    double fa, fb;
+    double x0[inter], f0[inter];
+    double err1 = 1;
+    double signal;
+    int i = 0;
+
+    printf("\n\n\t\t Enter with value of a: ");
+    scanf("%lf", &a);
+    printf("\n\n\t\t Enter with value of b: ");
+    scanf("%lf", &b);
+
+    fa = f(a);
+    fb = f(b);
+    signal = fa*fb;
+
+    if(signal < 0){
+        printf("\n\n\t\t INT \t\t A \t\t\t F(A) \t\t\t B \t\t\t F(B) \t\t\t XK \t\t\t F(XK) \t\t\t ERR", i, a, fa, b, fb, x0[i], f0[i], err1);
+        while (err1 > err && inter > i){
+            x0[i] = xkFalsePosition(a, b); 
+            f0[i] = f(x0[i]);
+            if(i > 0){
+                err1 = relativeError(x0[i], x0[i-1]);
+            }
+            else{
+                err1 = 0;
+            }
+            printf("\n\n\t\t %d \t\t %lf \t\t %lf \t\t %lf \t\t %lf \t\t %lf \t\t %lf \t\t %lf", i, a, fa, b, fb, x0[i], f0[i], err1);
+            if(f0[i]*fa < 0){
+                b = x0[i];
+                fb = f0[i];
+            }
+            else if(f0[i]*fb < 0){
+                a = x0[i];
+                fa = f0[i];
+            }
+            if(i == 0){
+                err1 = 1;
+            }
+            i++;    
+        }
+        if (err1 > err){
+            printf("\n\n\n");
+            printf("\n\n\t\t Number of interactions wasn't enough");
+        }
+        printf("\n\n\n");
+        printf("\n\t\t Result: %lf", x0[i-1]);
+        printf("\n\t\t Interactions: %d", i);
+        printf("\n\t\t Error: %lf", err1);
+        printf("\n\n\n");
+        printf("\t\t Press any key to continue ");
+        getchar();
+        getchar();
+    }
+    else{
+        printf("\n\n\t\t Interval does not respect the Intermediate Value Theorem");
+        Sleep(3500);
+    }
+}
+
 int main(){
     int choose;
     do{
@@ -180,7 +241,8 @@ int main(){
             break;
         
         case 2:
-
+            falsePosition();
+            system("cls");
             break;
 
         case 3:
